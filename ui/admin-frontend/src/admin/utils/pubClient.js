f53ad8043e41e8c034bba6273015740ea947465c
Martin Buhr <martin@tyk.io>
2024-11-06 13:47:12 +1300

    url and port shenanigans

--- ui/admin-frontend/src/admin/utils/pubClient.js
+++ ui/admin-frontend/src/admin/utils/pubClient.js
@@ -1,13 +1,19 @@
 import axios from "axios";
-import config from "../../config";
+import { getConfig } from "../../config";
 
-const pubClient = axios.create({
-  baseURL: config.API_BASE_URL,
-  withCredentials: true, // This is important for handling cookies
-});
+const createPubClient = () => {
+  const config = getConfig();
+  return axios.create({
+    baseURL: config.API_BASE_URL,
+    withCredentials: true,
+  });
+};
+
+let pubClient = createPubClient();
 
 // Function to fetch CSRF token
 const fetchCSRFToken = async () => {
+  const config = getConfig();
   try {
     const response = await axios.get(`${config.API_BASE_URL}/csrf-token`, {
       withCredentials: true,
@@ -34,5 +40,10 @@ pubClient.interceptors.request.use(
     return Promise.reject(error);
   },
 );
+
+// Export a function to reinitialize the client with updated config
+export const reinitializePubClient = () => {
+  pubClient = createPubClient();
+};
 
 export default pubClient;
