078824e09bbc4926f015f67241223e75aa0d668a
Leonid Bugaev <leonsbox@gmail.com>
2025-02-12 13:41:14 +0300

    Add missing API definition

--- api/api.go
+++ api/api.go
@@ -25,6 +25,25 @@ import (
 	"github.com/gorilla/csrf"
 )
 
+// @title           Midsommar API
+// @version         1.0
+// @description     This is the API for the Midsommar user and group management system.
+// @termsOfService  http://swagger.io/terms/
+
+// @contact.name   API Support
+// @contact.url    http://www.swagger.io/support
+// @contact.email  support@swagger.io
+
+// @license.name  Apache 2.0
+// @license.url   http://www.apache.org/licenses/LICENSE-2.0.html
+
+// @host      localhost:8080
+// @BasePath  /api/v1
+
+// @securityDefinitions.apikey BearerAuth
+// @in header
+// @name Authorization
+
 type API struct {
 	service             *services.Service
 	router              *gin.Engine
