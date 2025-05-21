ae723d5a0c70cb5d41bbee2149fb694e648f4293
Florencia Caballero <66144664+MFCaballero@users.noreply.github.com>
2025-05-21 11:22:58 +0200

    [TAS-19] Refactor teams Groups View UI (#159)
    * progress
    
    * sync BE
    
    * enhancements
    
    * sync BE
    
    * tests

---
+++ ui/admin-frontend/src/admin/pages/groups/utils/catalogueBadgeConfig.js
@@ -0,0 +1,16 @@
+const catalogueBadgeConfigs = {
+  llm: {
+    textColor: "text.primary",
+    bgColor: "background.buttonPrimaryOutlineHover"
+  },
+  data: {
+    textColor: "text.primary",
+    bgColor: "background.surfaceBrandDefaultDashboard"
+  },
+  tool: {
+    textColor: "text.primary",
+    bgColor: "background.surfaceBrandDefaultPortal"
+  }
+};
+
+export default catalogueBadgeConfigs;
