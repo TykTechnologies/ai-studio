3e63a49cde616b3f09cd345006e5f9bb78f2735e
María Florencia Caballero <66144664+MFCaballero@users.noreply.github.com>
2025-06-13 14:13:03 +0200

    fixes

--- models/group.go
+++ models/group.go
@@ -290,7 +290,7 @@ func (g *Groups) SearchByTerm(db *gorm.DB, term string, pageSize int, pageNumber
 
 	if term != "" {
 		searchTerm := "%" + term + "%"
-		query = query.Where("name LIKE ?", searchTerm)
+		query = query.Where("LOWER(name) LIKE LOWER(?)", searchTerm)
 	}
 
 	for _, preload := range preloads {
--- models/models.go
+++ models/models.go
@@ -23,7 +23,7 @@ func fixOAuthClientUserIDConstraint(db *gorm.DB) error {
 			AND table_name = 'o_auth_clients'
 		)
 	`).Scan(&constraintExists).Error
-	
+
 	if err != nil {
 		return err
 	}
@@ -53,13 +53,8 @@ func fixOAuthClientUserIDConstraint(db *gorm.DB) error {
 }
 
 func InitModels(db *gorm.DB) error {
-	// Fix OAuth client user_id constraint for PostgreSQL
-	err := fixOAuthClientUserIDConstraint(db)
-	if err != nil {
-		return err
-	}
-
-	err = db.AutoMigrate(
+	// First run AutoMigrate to create all tables
+	err := db.AutoMigrate(
 		&User{},      //Done
 		&Group{},     //Done
 		&LLM{},       //Done
@@ -86,6 +81,15 @@ func InitModels(db *gorm.DB) error {
 		&AccessToken{},
 		&PendingOAuthRequest{},
 	)
+	if err != nil {
+		return err
+	}
+
+	// Then fix OAuth client user_id constraint for PostgreSQL
+	err = fixOAuthClientUserIDConstraint(db)
+	if err != nil {
+		return err
+	}
 
 	err = db.Table("group_catalogues").AutoMigrate(&struct {
 		GroupID     uint `gorm:"primaryKey"`
