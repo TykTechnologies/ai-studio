1b314b6aab5013dbb6f96f313ea0d54e94b1bd66
Vlad Zabolotnyi <109525963+vladzabolotnyi@users.noreply.github.com>
2026-02-12 18:29:32 -0800

    fix: update google_ai auth rules (#290)
    * feat: add clearing up of Authorization header and let separate method attach vendor auth values
    
    * fix: always attach header and key if provided to avoid cases when request contains only Authorization header
    
    * fix: move loadResource() call after mockUpstream is initialized to set valid upstream url
    
    * test: add general int test for google_ai with todos
    
    ---------
    
    Co-authored-by: Vlad Zabolotnyi <vlad.z@tyk.io>

--- proxy/proxy_request_test.go
+++ proxy/proxy_request_test.go
@@ -50,10 +50,6 @@ func TestLLMRequestHandling(t *testing.T) {
 	err = db.Create(llm).Error
 	require.NoError(t, err)
 
-	// Force reload
-	err = proxy.loadResources()
-	require.NoError(t, err)
-
 	// Mock upstream
 	mockUpstream := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		body, err := io.ReadAll(r.Body)
@@ -77,6 +73,10 @@ func TestLLMRequestHandling(t *testing.T) {
 
 	llm.APIEndpoint = mockUpstream.URL
 	err = db.Save(llm).Error
+	require.NoError(t, err)
+
+	// Force reload with mockUpstream URL added
+	err = proxy.loadResources()
 	require.NoError(t, err)
 
 	proxy.credValidator.RegisterValidator(string(models.MOCK_VENDOR), func(r *http.Request) (string, error) {
@@ -135,4 +135,123 @@ func TestLLMRequestHandling(t *testing.T) {
 
 func TestDatasourceRequestHandling(t *testing.T) {
 	// ...
+}
+
+// TODO:
+// 1. Add case with processing `key` query param and x-goog-api-key header
+// 2. Make sure that valid Authorization header is enough to proxy Google AI requests
+// 3. Test all proxy routes including /ai/
+// 4. Add assertion to check that mockGAPIServer doesn't receive Authorization header if provided
+
+func TestGoogleAIRequestHandling(t *testing.T) {
+	db, cancel := setupTest(t)
+	defer tearDownTest(db, cancel)
+
+	mockGoogleAIServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+
+		response := `
+		{
+				"candidates": [
+						{
+								"content": {
+										"parts": [
+												{
+														"text": "Hello! How can I help you today?"
+												}
+										],
+										"role": "model"
+								},
+								"finishReason": "STOP",
+								"index": 0
+						}
+				],
+				"usageMetadata": {
+						"promptTokenCount": 3,
+						"candidatesTokenCount": 9,
+						"totalTokenCount": 227,
+						"promptTokensDetails": [
+								{
+										"modality": "TEXT",
+										"tokenCount": 3
+								}
+						],
+						"thoughtsTokenCount": 215
+				},
+				"modelVersion": "gemini-2.5-flash",
+				"responseId": "tfONacGUIq-D28oPjPSgsAs"
+		}
+		`
+
+		_, err := w.Write([]byte(response))
+		require.NoError(t, err)
+	}))
+	defer mockGoogleAIServer.Close()
+
+	service := services.NewService(db)
+	notificationSvc := services.NewTestNotificationService(db)
+	budgetService := budget.NewService(db, notificationSvc)
+	proxy := NewProxy(service, &Config{Port: 9999}, budgetService)
+	require.NotNil(t, proxy)
+
+	user := &models.User{ID: 1, Email: "test@example.com"}
+	db.Create(user)
+
+	googleAILLM := &models.LLM{
+		Model:        gorm.Model{ID: 1},
+		Name:         "gemini-api",
+		Vendor:       models.GOOGLEAI,
+		DefaultModel: "gemini-1.5-flash",
+		Active:       true,
+		APIEndpoint:  mockGoogleAIServer.URL,
+	}
+	err := db.Create(googleAILLM).Error
+	require.NoError(t, err)
+
+	app := &models.App{
+		Model:  gorm.Model{ID: 1},
+		LLMs:   []models.LLM{*googleAILLM},
+		UserID: user.ID,
+	}
+	db.Create(app)
+
+	cred := &models.Credential{Model: gorm.Model{ID: 1}, Secret: "valid-api-token", Active: true}
+	db.Create(cred)
+
+	app.CredentialID = cred.ID
+	app.LLMs = []models.LLM{*googleAILLM}
+	db.Save(app)
+
+	err = proxy.loadResources()
+	require.NoError(t, err)
+
+	r := mux.NewRouter()
+	finalHandler := proxy.credValidator.Middleware(
+		proxy.streamDetectionMiddleware(
+			http.HandlerFunc(proxy.handleUnifiedLLMRequest),
+		),
+	)
+	r.Handle("/llm/call/{llmSlug}/{rest:.*}", finalHandler)
+	proxySrv := httptest.NewServer(r)
+	defer proxySrv.Close()
+
+	reqBody := []byte(`{"model": "gemini-1.5-flash", "messages": [{"role": "user", "content": "Hello, world!"}]}`)
+	url := fmt.Sprintf("%s/llm/call/%s/v1beta/models/gemini-1.5-flash:generateContent", proxySrv.URL, googleAILLM.Name)
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(reqBody))
+	assert.NoError(t, err)
+	req.Header.Set("Authorization", "Bearer valid-api-token")
+	req.Header.Set("Content-Type", "application/json")
+
+	resp, err := http.DefaultClient.Do(req)
+	require.NoError(t, err)
+	defer func() {
+		err := resp.Body.Close()
+		assert.NoError(t, err)
+	}()
+
+	assert.Equal(t, http.StatusOK, resp.StatusCode)
+	respBody, _ := io.ReadAll(resp.Body)
+	assert.Contains(t, string(respBody), "Hello! How can I help you today?")
+	// TODO: Add more assertions
 }
